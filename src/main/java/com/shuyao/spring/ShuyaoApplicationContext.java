e9847976506afc297431f6e8632dc05fdc856713
fsynotfound <shuyaofan.coder@gmail.com>
2023-08-11 16:39:06 +0800

    手写源码实现spring启动流程

--- src/main/java/com/shuyao/spring/ShuyaoApplicationContext.java
+++ src/main/java/com/shuyao/spring/ShuyaoApplicationContext.java
@@ -1,10 +1,16 @@
 package com.shuyao.spring;
 
+import java.lang.annotation.Annotation;
+
 public class ShuyaoApplicationContext {
     private Class configClass;
 
     public ShuyaoApplicationContext(Class configClass) { //构造方法
         this.configClass = configClass;
+        //解析配置类
+        ComponentScan annotation = (ComponentScan) configClass.getAnnotation(ComponentScan.class);
+        String path=annotation.value(); //扫描路径
+        System.out.println(path); //打印出com.shuyao.test
     }
 
     public Object getBean(String beanName) {
